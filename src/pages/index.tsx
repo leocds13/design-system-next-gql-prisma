e7278aa930e313e8d6011372a137d879af54eed0
Leonardo Santos <leonardo.santos@iob.com.br>
2023-01-12 12:54:12 -0300

    docs(fileStruct): basics of structurer and documentations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useCallback, useState } from "react";
-import { trpc } from "@/utils/trpc";
+import { trpc } from "@/libs/trpc";
 
 import {
   Card,
